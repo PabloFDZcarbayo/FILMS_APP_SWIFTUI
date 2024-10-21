ee05c184b14e6e5063aee1401d089f688f0b3732
Pablo Lakers 1980 <149001800+PabloFDZcarbayo@users.noreply.github.com>
2024-10-22 00:56:07 +0200

    Initial Commit

---
+++ FILM APP_SwiftUI/FILM_APP_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FILM_APP_SwiftUIApp.swift
+//  FILM APP_SwiftUI
+//
+//  Created by Pablo  on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FILM_APP_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
